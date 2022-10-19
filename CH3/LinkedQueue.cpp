dc100552f7d1c9311ca07bdb653a884b21ec56d1
Neil-Chen <v009008777@hotmail.com>
2022-10-19 08:55:37 +0800

    feat: add LinkedQueue implementation

--- CH3/LinkedQueue.cpp
+++ CH3/LinkedQueue.cpp
@@ -3,3 +3,87 @@
 //
 
 #include "LinkedQueue.h"
+
+template<class T>
+void LinkedQueue<T>::makeEmpty() {
+    LinkedListNode<T> *p = front;
+    while (p != nullptr) {
+        LinkedListNode<T> *q = p;
+        p = p->next;
+        delete q;
+    }
+    front = nullptr;
+    rear = nullptr;
+}
+
+template<class T>
+LinkedQueue<T>::~LinkedQueue() {
+    makeEmpty();
+}
+
+template<class T>
+LinkedQueue<T>::LinkedQueue() {
+    front = nullptr;
+    rear = nullptr;
+}
+
+template<class T>
+bool LinkedQueue<T>::enqueue(const T value) {
+    auto *p = new LinkedListNode<T>;
+    p->data = value;
+    p->next = nullptr;
+    if (front == nullptr) {
+        front = p;
+        rear = p;
+    } else {
+        rear->next = p;
+        rear = p;
+    }
+    return true;
+}
+
+template<class T>
+T LinkedQueue<T>::dequeue() {
+    if (front == nullptr) {
+        throw "Queue is empty";
+    }
+    LinkedListNode<T> *p = front;
+    front = front->next;
+    T value = p->data;
+    delete p;
+    return value;
+}
+
+template<class T>
+T LinkedQueue<T>::getFront() const {
+    if (front == nullptr) {
+        throw "Queue is empty";
+    }
+    return front->data;
+}
+
+template<class T>
+bool LinkedQueue<T>::isEmpty() const {
+    return front == nullptr;
+}
+
+template<class T>
+int LinkedQueue<T>::getSize() const {
+    int length = 0;
+    LinkedListNode<T> *p = front;
+    while (p != nullptr) {
+        length++;
+        p = p->next;
+    }
+    return length;
+}
+
+template<class T>
+std::ostream &operator<<(std::ostream &os, const LinkedQueue<T> &queue) {
+    LinkedListNode<T> *p = queue.front;
+    while (p != nullptr) {
+        os << p->data << " ";
+        p = p->next;
+    }
+    return os;
+}
--- CH3/LinkedQueue.h
+++ CH3/LinkedQueue.h
@@ -5,10 +5,28 @@
 #ifndef CH3_LINKEDQUEUE_H
 #define CH3_LINKEDQUEUE_H
 
+#include "../CH2/SinglyLinkedList.h"
+#include "Queue.h"
 
-class LinkedQueue {
+#include <iostream>
 
+template<class T>
+class LinkedQueue: public Queue<T> {
+private:
+    LinkedListNode<T> *front = nullptr;
+    LinkedListNode<T> *rear = nullptr;
+
+public:
+    LinkedQueue();
+    ~LinkedQueue();
+
+    bool enqueue(const T value);
+    T dequeue();
+    T getFront() const;
+    bool isEmpty() const;
+    void makeEmpty();
+    int getSize() const;
+    friend std::ostream &operator<<(std::ostream &os, const LinkedQueue<T> &queue);
 };
-
 
 #endif //CH3_LINKEDQUEUE_H
