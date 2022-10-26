2b2437640ed3f64a0d92b0f74fde86bef1de0095
Neil-Chen <v009008777@hotmail.com>
2022-10-26 21:13:20 +0800

    feat: initial ch4 string

---
+++ CH4/String.cpp
@@ -0,0 +1,5 @@
+//
+// Created by yn on 26/10/2022.
+//
+
+#include "String.h"
---
+++ CH4/String.h
@@ -0,0 +1,14 @@
+//
+// Created by yn on 26/10/2022.
+//
+
+#ifndef CH3_STRING_H
+#define CH3_STRING_H
+
+
+class String {
+
+};
+
+
+#endif //CH3_STRING_H
